f2268372ad5bf9f3546e51e6bf0893c33c74c878
Alexandre Bossard <alexandre.bossard@tanker.io>
2020-05-22 21:25:05 +0200

    fix: Allow stream and executor

--- libs/fetchpp/include/fetchpp/core/detail/http_stable_async.hpp
+++ libs/fetchpp/include/fetchpp/core/detail/http_stable_async.hpp
@@ -1,16 +1,40 @@
 #pragma once
 
-#include <fetchpp/core/detail/async_http_result.hpp>
-
+#include <boost/asio/is_executor.hpp>
 #include <boost/beast/core/async_base.hpp>
+#include <boost/beast/core/stream_traits.hpp>
 
 #include <fetchpp/alias/beast.hpp>
+#include <fetchpp/alias/net.hpp>
+
+#include <type_traits>
 
 namespace fetchpp::detail
 {
-template <typename AsyncStream, typename Response, typename CompletionToken>
-using stable_async_t = beast::stable_async_base<
-    detail::async_http_completion_handler_t<CompletionToken, Response>,
-    typename AsyncStream::executor_type>;
+template <typename Executor, typename CompletionHandler, typename Enable = void>
+struct stable_async_t;
+
+template <typename Executor, typename CompletionHandler>
+struct stable_async_t<Executor,
+                      CompletionHandler,
+                      std::enable_if_t<net::is_executor<Executor>::value>>
+  : beast::stable_async_base<CompletionHandler, Executor>
+{
+  using beast::stable_async_base<CompletionHandler,
+                                 Executor>::stable_async_base;
+};
+
+template <typename AsyncStream, typename CompletionHandler>
+struct stable_async_t<
+    AsyncStream,
+    CompletionHandler,
+    std::enable_if_t<beast::is_async_stream<AsyncStream>::value>>
+  : beast::stable_async_base<CompletionHandler,
+                             typename AsyncStream::executor_type>
+{
+  using beast::stable_async_base<
+      CompletionHandler,
+      typename AsyncStream::executor_type>::stable_async_base;
+};
 
 }
