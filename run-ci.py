e5c813e14b0c251aeb450708f0709426f4f87f33
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2020-07-24 11:48:05 +0200

    ci: rename Python package

--- run-ci.py
+++ run-ci.py
@@ -4,31 +4,34 @@ import sys
 
 from path import Path
 
-import ci
-import ci.cpp
-import ci.conan
-import ci.git
+import tankerci
+import tankerci.cpp
+import tankerci.conan
+import tankerci.git
 
 
 def build_and_test(profile: str, coverage: bool) -> None:
-    built_path = ci.cpp.build(profile, make_package=True, coverage=coverage)
-    ci.cpp.check(built_path, coverage=coverage)
+    built_path = tankerci.cpp.build(profile, make_package=True, coverage=coverage)
+    tankerci.cpp.check(built_path, coverage=coverage)
 
 
 def deploy() -> None:
-    git_tag = os.environ["CI_COMMIT_TAG"] 
-    version = ci.version_from_git_tag(git_tag)
-    ci.bump_files(version)
-    ci.cpp.build_recipe(
-        Path.getcwd(),
-        conan_reference=f"fetchpp/{version}@tanker/stable",
-        upload=True
+    git_tag = os.environ["CI_COMMIT_TAG"]
+    version = tankerci.version_from_git_tag(git_tag)
+    tankerci.bump_files(version)
+    tankerci.cpp.build_recipe(
+        Path.getcwd(), conan_reference=f"fetchpp/{version}@tanker/stable", upload=True
     )
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--isolate-conan-user-home", action="store_true", dest="home_isolation", default=False)
+    parser.add_argument(
+        "--isolate-conan-user-home",
+        action="store_true",
+        dest="home_isolation",
+        default=False,
+    )
     subparsers = parser.add_subparsers(title="subcommands", dest="command")
 
     build_and_test_parser = subparsers.add_parser("build-and-test")
@@ -38,16 +41,16 @@ def main() -> None:
     subparsers.add_parser("deploy")
     subparsers.add_parser("mirror")
 
-    ci.conan.update_config()
+    tankerci.conan.update_config()
 
     args = parser.parse_args()
     if args.home_isolation:
-        ci.conan.set_home_isolation()
+        tankerci.conan.set_home_isolation()
 
     if args.command == "build-and-test":
         build_and_test(args.profile, args.coverage)
     elif args.command == "mirror":
-        ci.git.mirror(github_url="git@github.com:TankerHQ/fetchpp", force=True)
+        tankerci.git.mirror(github_url="git@github.com:TankerHQ/fetchpp", force=True)
     elif args.command == "deploy":
         deploy()
 
