6dc79c6ad0b345138eaf55499a6b71cdc90c7ca1
Alexandre Bossard <alexandre.bossard@tanker.io>
2020-09-02 19:46:57 +0200

    feat: Hello tunnel transport

---
+++ libs/fetchpp/include/fetchpp/core/tunnel_transport.hpp
@@ -0,0 +1,282 @@
+#pragma once
+
+#include <fetchpp/core/basic_transport.hpp>
+#include <fetchpp/core/detail/close_ssl.hpp>
+#include <fetchpp/core/endpoint.hpp>
+#include <fetchpp/core/process_one.hpp>
+
+#include <boost/asio/compose.hpp>
+#include <boost/asio/executor.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/context.hpp>
+#include <boost/beast/core/multi_buffer.hpp>
+#include <boost/beast/core/tcp_stream.hpp>
+#include <boost/beast/http/empty_body.hpp>
+#include <boost/beast/http/message.hpp>
+#include <boost/beast/http/string_body.hpp>
+#include <boost/beast/ssl/ssl_stream.hpp>
+
+#include <fetchpp/alias/beast.hpp>
+#include <fetchpp/alias/error_code.hpp>
+#include <fetchpp/alias/net.hpp>
+#include <fetchpp/alias/ssl.hpp>
+#include <fetchpp/alias/tcp.hpp>
+
+namespace fetchpp
+{
+class tunnel_async_transport;
+
+namespace detail
+{
+struct http_messages
+{
+  using body_t = beast::http::empty_body;
+  using request_t = beast::http::request<body_t>;
+  using response_t = beast::http::response<body_t>;
+
+  http_messages(request_t req) : request(std::move(req)), response()
+  {
+  }
+  request_t request;
+  response_t response;
+};
+
+template <typename Transport>
+struct async_tunnel_connect_op
+{
+  tunnel_endpoint const& endpoint_;
+  Transport& transport_;
+  std::unique_ptr<http_messages> transaction_ = {};
+  net::coroutine coro_ = {};
+
+  template <typename Self>
+  void operator()(Self& self,
+                  error_code ec = {},
+                  tcp::resolver::results_type::endpoint_type = {})
+  {
+    if (ec)
+    {
+      transport_.cancel_timer();
+      self.complete(ec);
+      return;
+    }
+    FETCHPP_REENTER(coro_)
+    {
+      transport_.set_running(true);
+      transport_.setup_timer();
+      FETCHPP_YIELD transport_.resolver_.async_resolve(
+          endpoint_.proxy.domain(),
+          std::to_string(endpoint_.proxy.port()),
+          net::ip::resolver_base::numeric_service,
+          std::move(self));
+      beast::get_lowest_layer(transport_)
+          .socket()
+          .set_option(net::ip::tcp::no_delay{true});
+      transaction_ = std::unique_ptr<http_messages>(new http_messages(
+          {beast::http::verb::connect, endpoint_.target.host(), 11}));
+      transaction_->request.set(beast::http::field::host,
+                                endpoint_.target.host());
+      transaction_->request.set(beast::http::field::proxy_connection,
+                                "Keep-Alive");
+      FETCHPP_YIELD async_process_one(transport_.next_layer().next_layer(),
+                                      transport_.buffer(),
+                                      transaction_->request,
+                                      transaction_->response,
+                                      std::move(self));
+      if (transaction_->response.result() != beast::http::status::ok)
+      {
+        // FIXME: fetchpp::errc::proxy_connection_refused or smth
+        self.complete(net::error::connection_refused);
+        return;
+      }
+      FETCHPP_YIELD transport_.next_layer().async_handshake(
+          net::ssl::stream_base::client, std::move(self));
+      transport_.cancel_timer();
+      self.complete(ec);
+    }
+  }
+
+  template <typename Self>
+  void operator()(Self& self,
+                  error_code ec,
+                  tcp::resolver::results_type results)
+  {
+    if (ec)
+    {
+      self.complete(ec);
+      return;
+    }
+    beast::get_lowest_layer(transport_).async_connect(results, std::move(self));
+  }
+};
+
+template <typename Transport>
+async_tunnel_connect_op(tunnel_endpoint, Transport&)
+    ->async_tunnel_connect_op<Transport>;
+
+template <typename CompletionToken>
+auto do_async_close(tunnel_async_transport& ts, CompletionToken&& token);
+}
+
+class tunnel_async_transport
+{
+  template <typename AsyncTransport>
+  friend struct detail::async_tunnel_connect_op;
+
+public:
+  using buffer_type = beast::multi_buffer;
+  using next_layer_type = beast::ssl_stream<beast::tcp_stream>;
+  using executor_type = typename next_layer_type::executor_type;
+  using next_layer_creator_sig = next_layer_type();
+  using next_layer_creator = std::function<next_layer_creator_sig>;
+
+  tunnel_async_transport(std::chrono::nanoseconds timeout,
+                         net::executor ex,
+                         net::ssl::context& ctx)
+    : stream_creator_([ex, &ctx]() { return next_layer_type(ex, ctx); }),
+      stream_(std::make_unique<next_layer_type>(stream_creator_())),
+      resolver_(next_layer().get_executor()),
+      timeout_(timeout)
+  {
+  }
+
+  template <typename MutableBufferSequence, typename ReadHandler>
+  auto async_read_some(MutableBufferSequence const& buffers,
+                       ReadHandler&& handler)
+  {
+    return next_layer().async_read_some(buffers,
+                                        std::forward<ReadHandler>(handler));
+  }
+
+  template <typename ConstBufferSequence, typename WriteHandler>
+  auto async_write_some(ConstBufferSequence const& buffers,
+                        WriteHandler&& handler)
+  {
+    return next_layer().async_write_some(buffers,
+                                         std::forward<WriteHandler>(handler));
+  }
+
+  template <typename CompletionToken>
+  auto async_connect(tunnel_endpoint const& endpoint, CompletionToken&& token)
+  {
+    return net::async_compose<CompletionToken, void(error_code)>(
+        detail::async_tunnel_connect_op{endpoint, *this}, token, *this);
+  }
+
+  template <typename CompletionToken>
+  auto async_close(CompletionToken&& token)
+  {
+    return net::async_compose<CompletionToken, void(error_code)>(
+        [this, coro_ = net::coroutine{}](auto& self,
+                                         error_code ec = {}) mutable {
+          FETCHPP_REENTER(coro_)
+          {
+            set_running(false);
+            this->setup_timer();
+            if (this->is_open())
+              FETCHPP_YIELD detail::do_async_close(*this, std::move(self));
+            else
+            {
+              this->resolver().cancel();
+              FETCHPP_YIELD net::post(
+                  beast::bind_front_handler(std::move(self)));
+            }
+            this->cancel_timer();
+            this->buffer().clear();
+            this->reset();
+            self.complete(ec);
+          }
+        },
+        token,
+        *this);
+  }
+
+  next_layer_type& next_layer()
+  {
+    return *stream_;
+  }
+
+  next_layer_type const& next_layer() const
+  {
+    return *stream_;
+  }
+
+  tcp::resolver& resolver()
+  {
+    return resolver_;
+  }
+
+  tcp::resolver const& resolver() const
+  {
+    return resolver_;
+  }
+
+  void setup_timer()
+  {
+    get_lowest_layer(next_layer()).expires_after(timeout_);
+  }
+
+  void cancel_timer()
+  {
+    get_lowest_layer(next_layer()).expires_never();
+  }
+
+  void reset()
+  {
+    stream_ = std::make_unique<next_layer_type>(stream_creator_());
+  }
+
+  bool is_open()
+  {
+    return beast::get_lowest_layer(next_layer()).socket().is_open();
+  }
+
+  auto const& timeout() const
+  {
+    return timeout_;
+  }
+
+  auto get_executor()
+  {
+    return next_layer().get_executor();
+  }
+
+  buffer_type const& buffer() const
+  {
+    return buffer_;
+  }
+
+  buffer_type& buffer()
+  {
+    return buffer_;
+  }
+
+  void set_running(bool r = true)
+  {
+    running_ = r;
+  }
+
+  bool is_running() const
+  {
+    return running_;
+  }
+
+private:
+  next_layer_creator stream_creator_;
+  buffer_type buffer_;
+  std::unique_ptr<next_layer_type> stream_;
+  tcp::resolver resolver_;
+  std::chrono::nanoseconds timeout_;
+  bool running_ = true;
+};
+
+namespace detail
+{
+template <typename CompletionToken>
+auto do_async_close(tunnel_async_transport& ts, CompletionToken&& token)
+{
+  return net::async_compose<CompletionToken, void(error_code)>(
+      detail::async_ssl_close_op{ts.next_layer()}, token, ts);
+}
+}
+}
---
+++ libs/fetchpp/tests/test_tunnel_transport.cpp
@@ -0,0 +1,114 @@
+#include <fetchpp/core/tunnel_transport.hpp>
+
+#include <fetchpp/http/request.hpp>
+#include <fetchpp/http/response.hpp>
+
+#include "helpers/format.hpp"
+#include "helpers/ioc_fixture.hpp"
+#include "helpers/match_exception.hpp"
+#include "helpers/test_domain.hpp"
+
+#include <fetchpp/core/detail/endpoint.hpp>
+
+#include <catch2/catch.hpp>
+
+#include <boost/asio/use_future.hpp>
+
+#include <chrono>
+
+using namespace std::chrono_literals;
+namespace ssl = fetchpp::net::ssl;
+using boost::asio::use_future;
+using namespace test::helpers::http_literals;
+
+using test::helpers::ioc_fixture;
+using URL = fetchpp::http::url;
+using test::helpers::HasErrorCode;
+
+TEST_CASE_METHOD(ioc_fixture, "connect timeouts", "[tunnel][http][timeout]")
+{
+  ssl::context ctx(ssl::context::tlsv12_client);
+  fetchpp::tunnel_async_transport ts(500ms, ex, ctx);
+  // this is a non routable ip
+  auto proxy = fetchpp::plain_endpoint("10.255.255.1", 8080);
+  auto const now = std::chrono::high_resolution_clock::now();
+  auto const url = URL("get"_https);
+  REQUIRE_THROWS_MATCHES(
+      ts.async_connect({proxy, fetchpp::detail::to_endpoint<true>(url)},
+                       boost::asio::use_future)
+          .get(),
+      boost::system::system_error,
+      HasErrorCode(boost::beast::error::timeout));
+  auto const end = std::chrono::high_resolution_clock::now();
+  REQUIRE((650ms) > (end - now));
+  REQUIRE_NOTHROW(ts.async_close(boost::asio::use_future).get());
+}
+
+TEST_CASE_METHOD(ioc_fixture, "transport one ssl", "[tunnel][https]")
+{
+  ssl::context ctx(ssl::context::tlsv12_client);
+  auto const url = URL("get"_https);
+  auto const proxy_url = URL(test::helpers::get_test_proxy());
+  auto const tunnel =
+      fetchpp::tunnel_endpoint{fetchpp::detail::to_endpoint<false>(proxy_url),
+                               fetchpp::detail::to_endpoint<true>(url)};
+
+  fetchpp::tunnel_async_transport ts(5s, ex, ctx);
+  REQUIRE_NOTHROW(ts.async_connect(tunnel, boost::asio::use_future).get());
+  auto const request =
+      fetchpp::http::make_request(fetchpp::http::verb::get, url);
+  fetchpp::http::response response;
+  REQUIRE_NOTHROW(
+      fetchpp::async_process_one(ts, request, response, boost::asio::use_future)
+          .get());
+  CHECK(response.result_int() == 200);
+  REQUIRE_NOTHROW(ts.async_close(boost::asio::use_future).get());
+}
+
+TEST_CASE_METHOD(ioc_fixture,
+                 "transport one ssl over timeout",
+                 "[tunnel][https][delay]")
+{
+  auto const url = URL("delay/5"_https);
+  ssl::context ctx(ssl::context::tlsv12_client);
+  auto const proxy_url = URL(test::helpers::get_test_proxy());
+  auto const tunnel =
+      fetchpp::tunnel_endpoint{fetchpp::detail::to_endpoint<false>(proxy_url),
+                               fetchpp::detail::to_endpoint<true>(url)};
+  fetchpp::tunnel_async_transport ts(1s, ex, ctx);
+  REQUIRE_NOTHROW(ts.async_connect(tunnel, boost::asio::use_future).get());
+  auto const request =
+      fetchpp::http::make_request(fetchpp::http::verb::get, url);
+  fetchpp::http::response response;
+  auto const now = std::chrono::high_resolution_clock::now();
+  REQUIRE_THROWS_MATCHES(
+      fetchpp::async_process_one(ts, request, response, boost::asio::use_future)
+          .get(),
+      boost::system::system_error,
+      HasErrorCode(boost::beast::error::timeout));
+  auto const end = std::chrono::high_resolution_clock::now();
+  REQUIRE(1.5s > end - now);
+}
+
+TEST_CASE_METHOD(ioc_fixture,
+                 "transport closes and reopens",
+                 "[tunnel][https][close]")
+{
+  auto const url = URL("get"_https);
+  ssl::context ctx(ssl::context::tlsv12_client);
+  auto const proxy_url = URL(test::helpers::get_test_proxy());
+  auto const tunnel =
+      fetchpp::tunnel_endpoint{fetchpp::detail::to_endpoint<false>(proxy_url),
+                               fetchpp::detail::to_endpoint<true>(url)};
+
+  fetchpp::tunnel_async_transport ts(5s, ex, ctx);
+  REQUIRE_NOTHROW(ts.async_connect(tunnel, boost::asio::use_future).get());
+  auto const request =
+      fetchpp::http::make_request(fetchpp::http::verb::get, url);
+  fetchpp::http::response response;
+  REQUIRE_NOTHROW(
+      fetchpp::async_process_one(ts, request, response, boost::asio::use_future)
+          .get());
+  REQUIRE_NOTHROW(ts.async_close(boost::asio::use_future).get());
+  REQUIRE_NOTHROW(ts.async_connect(tunnel, boost::asio::use_future).get());
+}
