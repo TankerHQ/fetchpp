39ca0042f2092bcd6db9f0e679ebf9131910d28c
Alexandre Bossard <alexandre.bossard@tanker.io>
2020-09-03 15:49:50 +0200

    fix: Move ssl close code around

---
+++ libs/fetchpp/include/fetchpp/core/detail/close_ssl.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <fetchpp/core/basic_transport.hpp>
+#include <fetchpp/core/detail/coroutine.hpp>
+
+#include <boost/asio/compose.hpp>
+#include <boost/beast/core/bind_handler.hpp>
+#include <boost/beast/core/error.hpp>
+#include <boost/beast/core/stream_traits.hpp>
+#include <boost/beast/ssl/ssl_stream.hpp>
+
+#include <fetchpp/alias/beast.hpp>
+#include <fetchpp/alias/error_code.hpp>
+#include <fetchpp/alias/tcp.hpp>
+
+namespace fetchpp
+{
+namespace detail
+{
+template <typename AsyncSSLStream>
+struct async_ssl_close_op
+{
+  static_assert(beast::is_async_stream<AsyncSSLStream>::value,
+                "AsyncStream type requirements not met");
+  AsyncSSLStream& stream_;
+  net::coroutine coro_ = {};
+
+  template <typename Self>
+  void operator()(Self& self, error_code ec = {})
+  {
+    FETCHPP_REENTER(coro_)
+    {
+      beast::get_lowest_layer(stream_).socket().cancel(ec);
+      // ignore the error
+      assert(!ec);
+      ec = {};
+      FETCHPP_YIELD net::post(beast::bind_front_handler(std::move(self)));
+      FETCHPP_YIELD stream_.async_shutdown(std::move(self));
+      if (ec == net::error::eof)
+        // http://stackoverflow.com/questions/25587403/boost-asio-ssl-async-shutdown-always-finishes-with-an-error
+        ec = {};
+      else if (ec == net::ssl::error::stream_truncated)
+        // lots of implementation do not respect the graceful shutdown
+        ec = {};
+      else if (ec == net::ssl::error::unspecified_system_error)
+        // we most probably canceled our stream, which is now in an
+        // inconsisent state. Forsake all etiquette, we want out anyway.
+        ec = {};
+      else if (ec == net::error::operation_aborted)
+        ec = {};
+      assert(!ec);
+      beast::close_socket(beast::get_lowest_layer(stream_));
+      self.complete(ec);
+    }
+  }
+};
+template <typename NextLayer>
+async_ssl_close_op(beast::ssl_stream<NextLayer>&)
+    ->async_ssl_close_op<beast::ssl_stream<NextLayer>>;
+}
+
+}
