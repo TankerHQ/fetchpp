f71bedc154fd8c9dcfcd571969a199b0457fd116
Alexandre Bossard <alexandre.bossard@tanker.io>
2020-11-04 15:25:04 +0100

    feat: add response::ok()

--- libs/fetchpp/include/fetchpp/http/response.hpp
+++ libs/fetchpp/include/fetchpp/http/response.hpp
@@ -15,5 +15,21 @@
 
 namespace fetchpp::http
 {
-using response = message<false, beast::multi_buffer>;
+
+template <typename DynamicBuffer>
+class basic_response : public message<false, DynamicBuffer>
+{
+public:
+  using base_t = message<false, DynamicBuffer>;
+  using base_t::operator=;
+  using base_t::base_t;
+
+  bool ok() const
+  {
+    return to_status_class(this->result()) ==
+           beast::http::status_class::successful;
+  }
+};
+
+using response = basic_response<beast::multi_buffer>;
 }
