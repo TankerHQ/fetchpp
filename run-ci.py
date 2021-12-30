5b94f4668cf7064e47cb065b33e02ba319627f97
Alexandre Bossard <alexandre.bossard@tanker.io>
2021-12-30 18:49:12 +0100

    feat: Change release process

--- run-ci.py
+++ run-ci.py
@@ -1,12 +1,17 @@
+import sys
 import argparse
-
+import re
 
 import tankerci
 import tankerci.conan
 import tankerci.cpp
+import cli_ui as ui
+
+from pathlib import Path
 
 
-def build_and_test(profile: str, coverage: bool, build_missing: bool) -> None:
+def build_and_test(profile: str,
+                   coverage: bool, build_missing: bool) -> None:
     build = ["never"]
 
     if build_missing:
@@ -16,6 +21,15 @@ def build_and_test(profile: str, coverage: bool, build_missing: bool) -> None:
         profile, make_package=True, build=build, coverage=coverage,
     )
     tankerci.cpp.check(built_path, coverage=coverage)
+
+
+def prepare_release():
+    infos = tankerci.conan.inspect(Path.cwd() / "conanfile.py")
+    version = infos["version"]
+    if re.match(r"\d+.\d+\d+(-(alpha|beta)\d+)?", version) is None:
+        ui.fatal("Bad version format")
+    with open('variables.env', 'w') as f:
+        f.write(f"FETCHPP_RELEASE_VERSION={infos['version']}")
 
 
 def main() -> None:
@@ -33,6 +47,8 @@ def main() -> None:
     build_and_test_parser.add_argument("--coverage", action="store_true")
     build_and_test_parser.add_argument("--build-missing", action="store_true")
 
+    subparsers.add_parser("prepare-release")
+
     args = parser.parse_args()
     if args.home_isolation:
         tankerci.conan.set_home_isolation()
@@ -40,6 +56,11 @@ def main() -> None:
 
     if args.command == "build-and-test":
         build_and_test(args.profile, args.coverage, args.build_missing)
+    elif args.command == "prepare-release":
+        prepare_release()
+    else:
+        parser.print_help()
+        sys.exit(1)
 
 
 if __name__ == "__main__":
